82879fe2a47d2cb241a448e505b386829d2535f1
Vikash Kumar Chaubey <230101120283@centurionuniv.edu.in>
2026-06-25 15:49:15 +0530

    app.jsx

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {TodoProvider} from './contexts'
 import './App.css'
 
@@ -20,6 +20,20 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+   localStorage.serItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
+  
 
   return (
     <TodoProvider values={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
